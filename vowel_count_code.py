526295c2c3555a4dc734cdab2ae0f59f1e383c88
TreverArnold <142126084+TreverArnold@users.noreply.github.com>
2023-09-04 11:19:04 -0500

    Cd wars vowel count (#4)
    * made code and test for a mutiply function
    
    * Changed name of test func to test_mutiply
    
    * function which returns numebr of vowels in a string
    
    * updated method used for vowel counting
    
    * vowels is now a set

---
+++ vowel_count_code.py
@@ -0,0 +1,7 @@
+def vowel_count(string):
+    vowels = set(['a','e','i','o','u','A','E','I','O','U'])
+    count = 0
+    for letter in string:
+        if letter in vowels:
+            count += 1
+    return(count)
