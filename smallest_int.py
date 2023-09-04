13b8c19e5d6521c34a40b73397f016557627aed6
Trever Arnold <treverarnold8@gmail.com>
2023-09-04 09:38:00 -0500

    takes in a strings and removes adjacent repetitions

--- smallest_int.py
+++
@@ -1,8 +0,0 @@
-def smallest_int(arr):
-    current = arr[0]
-
-    for num in arr:
-        if num < current:
-            current = num
-    
-    return current
--- smallest_int_test.py
+++
@@ -1,5 +0,0 @@
-from smallest_int import smallest_int
-
-def test():
-    assert smallest_int([9,6,3,21,3,5,7]) == 3
-    assert smallest_int([91,62,324,21,31,52,43]) == 21
