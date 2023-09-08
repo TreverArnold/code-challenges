625c91ece608fb74646fbe88c01109f866b4a7fa
Trever Arnold <treverarnold8@gmail.com>
2023-09-08 11:50:27 -0500

    edited variably names

--- break_camel_case.py
+++ break_camel_case.py
@@ -1,7 +1,7 @@
 import re
-def break_camel_case(s):
-    str = ''
-    s = re.split(r'(?=[A-Z])', s)
-    for word in s:
-        str += word + ' '
-    return str.rstrip()
+def break_camel_case(camel):
+    result = ''
+    camel = re.split(r'(?=[A-Z])', camel)
+    for word in camel:
+        result += word + ' '
+    return result.rstrip()
