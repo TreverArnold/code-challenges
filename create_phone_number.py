d3424beff7b160b89fe3aaee7f12517f72327b53
Trever Arnold <treverarnold8@gmail.com>
2023-09-08 11:46:05 -0500

    adjusted varible names and created more efficent tests

--- create_phone_number.py
+++ create_phone_number.py
@@ -1,3 +1,3 @@
-def create_phone_number(n):
-    result_str = ''.join([str(digit) for digit in n])
+def create_phone_number(num):
+    result_str = ''.join([str(digit) for digit in num])
     return f'({result_str[:3]}) {result_str[3:6]}-{result_str[6:]}'
--- create_phone_number_test.py
+++ create_phone_number_test.py
@@ -2,4 +2,4 @@ from create_phone_number import create_phone_number
 
 def test_create_phone_number():
     assert  create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9]) == '(123) 456-789'
-    assert  create_phone_number([1, 'b', 3, 4, 5, 'a', 7, 8, 9]) == '(1b3) 45a-789'
+    assert  create_phone_number([1, 5, 3, 1, 9, 6, 7, 2, 4, 8]) == '(153) 196-7248'
