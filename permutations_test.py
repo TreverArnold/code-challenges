2c0ca7c5aec549f6f84099a9c673f2418d1b9007
Trever Arnold <treverarnold8@gmail.com>
2023-09-19 12:10:33 -0500

    altered the test to use sets

--- permutations_test.py
+++ permutations_test.py
@@ -2,13 +2,12 @@ from permutations import permutations
 
 
 def test_permutaions():
-    assert permutations("a") == ["a"]
-    assert all(perm in ["ab", "ba"] for perm in permutations("ab"))
+    assert all(perm in {"ab", "ba"} for perm in permutations("ab"))
     assert all(
-        perm in ["abc", "acb", "cba", "cab", "bca", "bac"]
+        perm in {"abc", "acb", "cba", "cab", "bca", "bac"}
         for perm in permutations("abc")
     )
     assert all(
-        perm in ["aabb", "abab", "abba", "baab", "baba", "bbaa"]
+        perm in {"aabb", "abab", "abba", "baab", "baba", "bbaa"}
         for perm in permutations("aabb")
     )
