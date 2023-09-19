d5deeea4f361e5a76a581cc4803cd5d0e06dff99
Trever Arnold <treverarnold8@gmail.com>
2023-09-19 13:40:20 -0500

    Moved nested functions into global space

--- sudoku.py
+++ sudoku.py
@@ -1,34 +1,34 @@
-def sudoku(puzzle):
-    def is_valid(num, row, col):
-        if num in puzzle[row]:
+def is_valid(num, row, col, puzzle):
+    if num in puzzle[row]:
+        return False
+
+    for i in range(9):
+        if puzzle[i][col] == num:
             return False
 
-        for i in range(9):
-            if puzzle[i][col] == num:
+    rowstart = 3 * (row // 3)
+    colstart = 3 * (col // 3)
+
+    for r in range(rowstart, rowstart + 3):
+        for c in range(colstart, colstart + 3):
+            if puzzle[r][c] == num:
                 return False
 
-        rowstart = 3 * (row // 3)
-        colstart = 3 * (col // 3)
+    return True
 
-        for r in range(rowstart, rowstart + 3):
-            for c in range(colstart, colstart + 3):
-                if puzzle[r][c] == num:
-                    return False
+def solve(puzzle):
+    for row in range(9):
+        for col in range(9):
+            if puzzle[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(num, row, col, puzzle):
+                        puzzle[row][col] = num
+                        if solve(puzzle):
+                            return True
+                        puzzle[row][col] = 0
+                return False
+    return True
 
-        return True
-
-    def solve():
-        for row in range(9):
-            for col in range(9):
-                if puzzle[row][col] == 0:
-                    for num in range(1, 10):
-                        if is_valid(num, row, col):
-                            puzzle[row][col] = num
-                            if solve():
-                                return True
-                            puzzle[row][col] = 0
-                    return False
-        return True
-
-    if solve():
+def sudoku(puzzle):
+    if solve(puzzle):
         return puzzle
