2a0684cb09d92a3bb6ca2f4b7b417a46812a2fba
Trever Arnold <treverarnold8@gmail.com>
2023-10-16 09:58:40 -0500

    fixed spacing

--- javaScript/find.smallest.js
+++ javaScript/find.smallest.js
@@ -1,17 +1,17 @@
 function smallest(num) {
-    let move = [num, 0, 0];
-    num = num.toString();
-    let smallestNum = num;
-    for (let i = 0; i < num.length; i++) {
-        for (let j = 0; j <= num.length; j++) {
-            let tempNum = num.split('');
-            let digit = tempNum.splice(i, 1)[0];
-            tempNum.splice(j, 0, digit);
+  let move = [num, 0, 0];
+  num = num.toString();
+  let smallestNum = num;
+  for (let i = 0; i < num.length; i++) {
+    for (let j = 0; j <= num.length; j++) {
+      let tempNum = num.split('');
+      let digit = tempNum.splice(i, 1)[0];
+      tempNum.splice(j, 0, digit);
 
-            let currentNum = parseInt(tempNum.join(''));
-            if (currentNum < smallestNum) {
-                smallestNum = currentNum;
-                move = [smallestNum, i, j];
+      let currentNum = parseInt(tempNum.join(''));
+      if (currentNum < smallestNum) {
+        smallestNum = currentNum;
+        move = [smallestNum, i, j];
             }
         }
     }
