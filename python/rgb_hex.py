c4455aa933de5c809a0a26e6cf1abe30348d4917
Trever Arnold <treverarnold8@gmail.com>
2023-10-19 09:43:11 -0500

    modified return

--- python/rgb_hex.py
+++ python/rgb_hex.py
@@ -2,4 +2,4 @@ def rgb(r, g, b):
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
-    return "{:02X}{:02X}{:02X}".format(r, g, b)
+    return f"{r:02X}{g:02X}{b:02X}"
