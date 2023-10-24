ac281d0595c9c4794c013f6645d902cc0ce87095
Trever Arnold <treverarnold8@gmail.com>
2023-10-24 11:02:22 -0500

    fixed formatting on test file

--- python/popping_blocks_test.py
+++ python/popping_blocks_test.py
@@ -2,9 +2,7 @@ from popping_blocks import pop_blocks
 
 
 def test_pop_blocks():
-    assert pop_blocks(['B', 'B', 'A', 'C', 'A', 'A', 'C']) == ['A']
-    assert pop_blocks(['B', 'B', 'A', 'C', 'A', 'A', 'C', 'B']) == ['A', 'B']
-    assert pop_blocks(['B', 'B', 'C', 'A', 'A', 'C']) == []
+    assert pop_blocks(["B", "B", "A", "C", "A", "A", "C"]) == ["A"]
+    assert pop_blocks(["B", "B", "A", "C", "A", "A", "C", "B"]) == ["A", "B"]
+    assert pop_blocks(["B", "B", "C", "A", "A", "C"]) == []
     assert pop_blocks([]) == []
-
-
